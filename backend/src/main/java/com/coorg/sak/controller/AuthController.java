707434a362ca2d867fad46ec0631a1f41db8d737
sakit333 <sak528264@gmail.com>
2024-11-27 15:23:41 +0530

    completed with new set

--- backend/src/main/java/com/coorg/sak/controller/AuthController.java
+++ backend/src/main/java/com/coorg/sak/controller/AuthController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin
+@CrossOrigin(origins = "*")
 public class AuthController {
     @Autowired
     private UserService userService;
