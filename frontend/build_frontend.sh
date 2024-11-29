fc36a63a7bb2883645a39dfb7a67a07ebe750e8c
sakit333 <sak528264@gmail.com>
2024-11-29 13:03:02 +0530

    gf

--- frontend/build_frontend.sh
+++ frontend/build_frontend.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
-echo "install glibc"
-rpm -q glibc
-sudo yum update glibc
-
-echo "glibc completed"
-echo "install node....."
-curl -sL https://rpm.nodesource.com/setup_20.x | sudo bash -
-sudo yum install -y nodejs
-
 echo "installation complete"
 
